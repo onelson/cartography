b426aa6b835c9ce2baef34e5950c485ded3504b9
Owen Nelson <onelson@gmail.com>
2015-08-11 00:39:28 -0700

    tiny version bump, setting server side only, and stubbing out the player tracking.

--- src/main/scala/com/theomn/cartography/CartographyMod.scala
+++ src/main/scala/com/theomn/cartography/CartographyMod.scala
@@ -1,29 +1,41 @@
 package com.theomn.cartography
 
-import com.theomn.cartography.web.controllers.Application
-import controllers.Assets
-import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEvent, FMLServerStoppingEvent}
+import net.minecraft.util.BlockPos
+import net.minecraftforge.fml.common.gameevent.PlayerEvent
+
+import scala.collection.JavaConversions._
+
 import org.apache.logging.log4j.LogManager
+
+import controllers.Assets
 import play.api.mvc.{Action, Results}
 import play.api.routing.sird._
 import play.core.server._
 
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEvent, FMLServerStoppingEvent}
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent
 
-@Mod(modid="Cartography", name="Cartography", version="0.0.0-SNAPSHOT", modLanguage="scala")
+import scala.collection.mutable
+
+
+@Mod(modid="Cartography", name="Cartography", version="0.0.1-SNAPSHOT", modLanguage="scala", serverSideOnly=true)
 object CartographyMod {
 
+  val moveThreshold: Int = 10
   val logger = LogManager.getLogger("Cartography")
 
   var server: Option[NettyServer] = None
+  val playerPositions = mutable.Map[EntityPlayer, BlockPos]()
+
+  // TODO: lookup conf details for the netty server
+  // TODO: prime the sqlite db
+  @Mod.EventHandler
+  def preInit(e: FMLPreInitializationEvent): Unit = {}
 
   @Mod.EventHandler
-  def preInit(e: FMLPreInitializationEvent) {
-    // TODO: lookup conf details for the netty server
- }
-
-  @Mod.EventHandler
-  def init(e: FMLInitializationEvent) {
+  def init(e: FMLInitializationEvent): Unit = {
     // TODO: use config (read during preInit) when creating server
     server = Some(NettyServer.fromRouter() {
       case GET(p"/") => Assets.versioned(path="/public", file="index.html")
@@ -33,8 +45,34 @@ object CartographyMod {
   }
 
   @Mod.EventHandler
-  def shutdown(e: FMLServerStoppingEvent) {
+  def shutdown(e: FMLServerStoppingEvent): Unit = {
     server.foreach(_.stop())
   }
+
+
+  @Mod.EventHandler
+  def onLogin(e: PlayerEvent.PlayerLoggedInEvent): Unit =
+    playerPositions.update(e.player, e.player.getPosition)
+
+  @Mod.EventHandler
+  def onLogout(e: PlayerEvent.PlayerLoggedOutEvent): Unit =
+    playerPositions.remove(e.player)
+
+  @Mod.EventHandler
+  def tick(e: WorldTickEvent): Unit =
+    for (p <- e.world.playerEntities.map(_.asInstanceOf[EntityPlayer])
+         if isMoving(p)) {
+      updateTile(p.getPosition)
+    }
+
+
+  def isMoving(player: EntityPlayer): Boolean = {
+    val lastPos = playerPositions(player)
+    val diff = scala.math.abs(lastPos.toLong - player.getPosition.toLong)
+    logger.debug(s"diff: $diff")
+    false
+  }
+
+  def updateTile(pos: BlockPos) = {}
 
 }
