02c54eaf41f045ff8afc44c2002ae2f29f6e28b5
Owen Nelson <onelson@gmail.com>
2015-08-21 21:09:59 -0700

    sketching the db bootstrapping

---
+++ src/main/scala/com/theomn/cartography/DB.scala
@@ -0,0 +1,22 @@
+package com.theomn.cartography
+
+import slick.driver.H2Driver.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+object DB {
+
+  // TODO: ensure schema is setup before handing back a connection
+  def get = Database.forConfig("h2mem")
+
+  /**
+   * Unsure this will actually be useful...
+   * @param q query to run
+   * @return Unit
+   */
+  def run(q: => Any) = try {
+    implicit val db = get
+    q
+  } finally db.close
+
+}
