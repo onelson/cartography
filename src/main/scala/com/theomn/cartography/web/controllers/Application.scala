a092586ef540ddfc12f2e0d1a2b056f3cdcd7832
Owen Nelson <onelson@gmail.com>
2015-08-29 01:51:41 -0700

    tile interaction endpoint is working now

--- src/main/scala/com/theomn/cartography/web/controllers/Application.scala
+++ src/main/scala/com/theomn/cartography/web/controllers/Application.scala
@@ -1,14 +1,16 @@
 package com.theomn.cartography.web.controllers
 
+import javax.xml.bind.DatatypeConverter
 
 import akka.actor.ActorSystem
 import akka.event.Logging
-import akka.http.scaladsl.model.{HttpResponse, ContentType, MediaType}
+import akka.http.scaladsl.model.{MediaTypes, HttpResponse, ContentType}
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
 import slick.driver.H2Driver.api._
@@ -21,48 +23,65 @@ object JsonProtocol extends DefaultJsonProtocol {
   implicit val tileFormat = jsonFormat4(DBTile.apply)
 }
 
-import com.theomn.cartography.Executors.threadpoolContext
+import com.theomn.cartography.Executors
 
 
 object Application extends App {
-
   import JsonProtocol._
-  implicit val system = ActorSystem("cartogarphy")
+  implicit val ctx = Executors.threadpoolContext
+  implicit val system = ActorSystem("cartogarphy-web")
   implicit val materializer = ActorMaterializer()
   val logger = Logging(system, getClass)
+  val db = DB.getConn
 
-  val route = path("tiles") {
-      get {
-        val db = DB.getConn
-        complete {
-          ToResponseMarshallable {
-            db.run(tiles.all.result)
-          }
+  val route = pathSingleSlash {
+    get{
+      complete {
+        <html>
+          <body>Hello world!</body>
+        </html>
+      }
+    }
+  } ~
+  (pathPrefix("tiles") & get) {
+    pathEnd {
+      complete {
+        ToResponseMarshallable {
+          db.run(tiles.all.result)
         }
       }
     } ~
-    path("tiles" / Segment / IntNumber / IntNumber / IntNumber) {
+    (path("grid") & get){
+      // TODO: display grid of tile pics
+      complete {
+        ToResponseMarshallable {
+          db.run(tiles.all.result)
+        }
+      }
+    } ~
+    (path(Segment / Segment / Segment / IntNumber) & get) {
       (worldName, col, row, zoomLevel) =>
-        get {
-          val q = tiles.
-            filter(_.col === col)
-            .filter(_.row === row)
+        complete {
+          val q = tiles
+            .filter(_.col === col.toInt)
+            .filter(_.row === row.toInt)
             .filter(_.zoomLevel === zoomLevel)
-
-
-          DB.getConn.run(q.result.headOption).map {
-
-            case Some(data) =>
-              val img = ???
-              val resp = HttpResponse(OK, entity=img)
-              complete(OK, resp.entity.withContentType(ContentType(MediaType.`image/png`)))
+          for {result <- DB.getConn.run(q.result.headOption)} yield result match {
+            case Some(tile: DBTile) =>
+              val img = DatatypeConverter.parseBase64Binary(tile.data)
+              val resp = HttpResponse(OK, entity = img)
+              val respEntity =
+                resp.entity.withContentType(ContentType(MediaTypes.`image/png`))
+              HttpResponse(OK, entity = respEntity)
             case _ =>
-              complete(NotFound, "tile not found")
+              HttpResponse(NotFound, entity = "tile not found")
           }
 
         }
 
     }
+
+  }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 9000)
 
